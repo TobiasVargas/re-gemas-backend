a6ec7406379c00f986db26514137c256e73765e5
TobiasVargas <tobiasdevargas@outlook.com>
2021-08-19 21:54:34 -0300

    wip

--- products/api/serializers.py
+++ products/api/serializers.py
@@ -2,15 +2,17 @@ from rest_framework import serializers
 from products.models import Category, SubCategory, Product
 
 
-class CategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Category
-        fields = '__all__'
-
-
 class SubCategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = SubCategory
+        fields = '__all__'
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    subcategories = SubCategorySerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Category
         fields = '__all__'
 
 
--- products/models.py
+++ products/models.py
@@ -8,13 +8,25 @@ class Category(models.Model):
     def __str__(self):
         return self.title
 
+    class Meta:
+        verbose_name = "Categoria"
+        ordering = ['id']
+
+    def subcategories(self):
+        return self.subcategory_set
+
 
 class SubCategory(models.Model):
     title = models.CharField(max_length=100)
     banner = models.ImageField(upload_to="banners_sub_category/")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.title
+        return self.category.title + " - " + self.title
+
+    class Meta:
+        verbose_name = "SubCategoria"
+        ordering = ['id']
 
 
 class Product(models.Model):
@@ -24,4 +36,14 @@ class Product(models.Model):
     subcategory = models.ForeignKey(SubCategory, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
-        return self.title
+        # if self.title:
+        #     return self.title
+        # if self.category:
+        #     return "Produto Categoria " + self.category.title
+        # if self.subcategory:
+        #     return "Produto Categoria " + self.subcategory.category.title + " - Subcategoria - " + self.subcategory.title
+        return "Produto " + str(self.id)
+
+    class Meta:
+        verbose_name = "Produto"
+        ordering = ['-id']
