7ad3fa42c38c29da5cc76ebe5b4772386ecea8bc
TobiasVargas <tobiasdevargas@outlook.com>
2021-09-01 21:51:49 -0300

    wip

--- settings/settings.py
+++ settings/settings.py
@@ -79,10 +79,21 @@ WSGI_APPLICATION = 'settings.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'regemasc_site',
+        'USER': 'regemasc_admin',
+        'PASSWORD': 'database1Qaz',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
